520dc003e211e42877820ed4e7de519870f48c14
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-09-08 12:15:54 +0200

    feat(repo): init repo

---
+++ src/main/java/com/example/springbootinov/SpringbootInovApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootinov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootInovApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootinov/SpringbootInovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootinov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInovApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
